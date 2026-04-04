1255c71b730990671f11bf1349ccf44f83b92e61
venk2994 <venkateshwaran.murugesan@hainsys.com>
2026-04-04 15:24:09 +0530

    Vercel backend

--- server/server.js
+++ server/server.js
@@ -103,6 +103,10 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production' || !process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
