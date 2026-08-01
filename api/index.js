bfd4259758131f08d6378a7028f20efa7dc515f2
Venkateshwaran11 <venkateshwaran867@gmail.com>
2026-08-01 18:07:41 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -6,11 +6,7 @@ const cors = require('cors');
 // const dns = require('node:dns');
 // dns.setServers(['1.1.1.1', '8.8.8.8']);
 const fs = require('fs');
-connectDB()
-const formRoutes = require('./_src/routes/formRoutes');
-const responseRoutes = require('./_src/routes/responseRoutes');
-const uploadRoutes = require('./_src/routes/uploadRoutes');
-const authRoutes = require('./_src/routes/registerandLoginRoutes')
+
 
 console.log("Server started at:", new Date().toISOString());
 console.log("PID:", process.pid);
@@ -45,6 +41,11 @@ await mongoose.connect(MONGODB_URI)
   .then(() => console.log('✅ Connected to MongoDB'))
   .catch(err => console.error('❌ MongoDB Connection Error:', err));
 }
+connectDB()
+const formRoutes = require('./_src/routes/formRoutes');
+const responseRoutes = require('./_src/routes/responseRoutes');
+const uploadRoutes = require('./_src/routes/uploadRoutes');
+const authRoutes = require('./_src/routes/registerandLoginRoutes')
 // --- API ROUTES ---
 app.use('/api/forms', formRoutes);
 app.use('/api/responses', responseRoutes);
