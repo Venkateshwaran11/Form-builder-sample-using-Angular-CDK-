7f9aeca42175a73178badaff3fa2c2a5d3fd2d99
Venkateshwaran11 <venkateshwaran867@gmail.com>
2026-09-10 20:48:12 +0530

    Add error handling and snackbar notifications; enhance dynamic form editing features

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,16 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { errorInterceptor } from './interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideAnimationsAsync(),
     provideHttpClient(
-      withInterceptors([authInterceptor])
-    ), provideAnimationsAsync()
+      withInterceptors([authInterceptor,errorInterceptor])
+    ), provideAnimationsAsync(),
+    MatSnackBarModule
   ]
 };
